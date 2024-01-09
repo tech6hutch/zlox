8a1ed5bf5734019610184566af366964472d6a26
Hutch <tech_hutch@verizon.net>
2024-01-08 22:42:57 -0500

    add the actual VM; also gitignore vim swap files

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,14 @@ const std = @import("std");
 const Chunk = @import("./chunk.zig").Chunk;
 const OpCode = @import("./chunk.zig").OpCode;
 const dbg = @import("./debug.zig");
+const VM = @import("./vm.zig").VM;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+
+    var vm = VM.init();
+
     var chunk = Chunk.init(allocator);
 
     try chunk.write_const(1.2, 123);
@@ -13,5 +17,8 @@ pub fn main() !void {
     try chunk.write_op_code(OpCode.op_return, 123);
 
     dbg.disassembleChunk(&chunk, "test chunk");
+    try vm.interpret(&chunk);
+
+    vm.deinit();
     chunk.free();
 }
---
+++ src/vm.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+const Chunk = @import("./chunk.zig").Chunk;
+const OpCode = @import("./chunk.zig").OpCode;
+const Value = @import("./value.zig").Value;
+const printValue = @import("./value.zig").printValue;
+
+const STACK_MAX: usize = 256;
+
+pub const VM = struct {
+    chunk: ?*Chunk,
+    ip: ?[*]u8,
+    stack: [STACK_MAX]Value,
+    stack_top: ?[*]Value,
+
+    pub fn init() VM {
+        var vm: VM = .{
+            .chunk = null,
+            .ip = null,
+            .stack = undefined,
+            .stack_top = null,
+        };
+        vm.resetStack();
+        return vm;
+    }
+    pub fn deinit(self: *VM) void {
+        _ = self;
+    }
+
+    pub fn interpret(self: *VM, chunk: *Chunk) InterpretError!void {
+        self.chunk = chunk;
+        self.ip = @ptrCast(chunk.code.items);
+        return self.run();
+    }
+
+    fn run(self: *VM) InterpretError!void {
+        while (true) {
+            const instruction = self.readByte();
+            switch (instruction) {
+                @intFromEnum(OpCode.op_constant), @intFromEnum(OpCode.op_constant_long) => {
+                    const constant = self.readConst();
+                    printValue(constant);
+                    std.debug.print("\n", .{});
+                },
+                @intFromEnum(OpCode.op_return) => {
+                    return;
+                },
+                else => {
+                    std.debug.panic("unknown opcode {d}", .{instruction});
+                },
+            }
+        }
+    }
+    inline fn readByte(self: *VM) u8 {
+        const byte = self.ip.?[0];
+        self.ip.? += 1;
+        return byte;
+    }
+    inline fn readConst(self: *VM) Value {
+        return self.chunk.?.constIdx(self.readByte());
+    }
+
+    fn resetStack(self: *VM) void {
+        self.stack_top = self.stack[0..];
+    }
+};
+
+pub const InterpretError = error{ CompileError, RuntimeError };
