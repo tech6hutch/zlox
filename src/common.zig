ce281dbe2934cde54a3c299a7672d13f1b2080cc
Hutch <tech_hutch@verizon.net>
2024-12-27 20:28:24 -0500

    actually zig didn't rename this

--- src/common.zig
+++ src/common.zig
@@ -1,4 +1,4 @@
 pub const DEBUG_PRINT_CODE = false;
 pub const DEBUG_TRACE_EXECUTION = false;
-pub const DEBUG_STRESS_GC = true;
-pub const DEBUG_LOG_GC = true;
+pub const DEBUG_STRESS_GC = false;
+pub const DEBUG_LOG_GC = false;
